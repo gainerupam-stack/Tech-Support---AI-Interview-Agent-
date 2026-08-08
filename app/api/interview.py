e2f417308f884339c5c04afb8dc5549280c34006
RishanEP08 <rishanepandey.pkt@gmail.com>
2026-08-08 21:56:36 +0530

    Add candidate api endpoint

--- app/api/interview.py
+++ app/api/interview.py
@@ -1,8 +1,22 @@
 from fastapi import APIRouter
 from app.models.request import InterviewRequest
 from app.services.interview_service import process
+from app.services.data_loader import load_candidates
 
 router = APIRouter()
+
+
+@router.get("/api/candidates")
+def get_candidates():
+
+    candidates = load_candidates()
+
+    return {
+        "candidates": [
+            candidate["member"]
+            for candidate in candidates["candidates"]
+        ]
+    }
 
 
 @router.post("/api/interview")
