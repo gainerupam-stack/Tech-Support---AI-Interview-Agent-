691df28c50b4a3440c6190aa6353b2a23da54b14
RishanEP08 <rishanepandey.pkt@gmail.com>
2026-08-08 23:16:39 +0530

    Connect Frontend to deploy backend

--- app/frontend1/src/api.js
+++ app/frontend1/src/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000"
+const API_URL = "https://tech-support-ai-interview-agent.onrender.com"
 
 export async function getCandidates() {
   const response = await fetch(`${API_URL}/api/candidates`)
